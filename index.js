c9f98a70ee473a477ad19c3ed67a568f13dba14b
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-06-22 22:22:08 +0330

    feat: add index js

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-const contentDiv = document.getElementById("app");
-if (location.hash === "") {
-  location.hash = "#home";
-}
-// location.hash = "#home";
-const getPage = (currentlyPage) => {
-  const pages = {
-    home: "This is the Home page. Welcome to my site.",
-    cart: "This page will describe what my site is about",
-    singlePage: "Contact me on this page if you have any questions",
-  };
-  return pages[currentlyPage];
-};
-const loadPage = () => {
-  const currentlyPage = location.hash.substr(1);
-  contentDiv.innerHTML = getPage(currentlyPage);
-};
-let globalEmail;
-const loginUser = (email, callBack) => {
-  setTimeout(() => callBack(email), 2000);
-};
-const loginUserCallback = (e) => {
-  console.log(e)
-}
-loginUser("daskl;dkas;ldas", loginUserCallback);
+export const root = document.getElementById("root");
 
-window.addEventListener("hashchange", loadPage);
+import { updateRoot } from "./updateRoot.js";
+
+updateRoot();
