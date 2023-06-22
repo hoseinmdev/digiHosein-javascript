6fbee98b9d76e1c582808a441c71123cc46e657a
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-06-22 22:23:42 +0330

    feat: add routes

---
+++ routesPath.js
@@ -0,0 +1,8 @@
+import homePage from "./pages/homePage.js";
+import cartPage from "./pages/cartPage.js";
+
+export const routesPath = {
+  home: homePage,
+  cart: cartPage,
+  singlePage: "Contact me on this page if you have any questions",
+};
