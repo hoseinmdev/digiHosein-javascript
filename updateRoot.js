9186e4c1041c7e8235530f744cae44c363956010
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-06-23 15:43:40 +0330

    feat: add fadeShow effect to children of layout

--- updateRoot.js
+++ updateRoot.js
@@ -5,6 +5,12 @@ export const updateRoot = () => {
   const currentHash = location.hash.split("#")[1];
   if (!location.hash) location.hash = "#home";
   root.innerHTML = routesPath[currentHash];
+  // console.log(root.childNodes[1])
+  const childrenOfSiteLayout =
+    document.querySelector(".siteLayout").childNodes[3];
+  setTimeout(() => {
+    childrenOfSiteLayout.classList.remove("noneShow");
+  }, 150);
 };
 
 window.addEventListener("hashchange", updateRoot);
