4977764d520e8bf8068e3079216d911a53c4641b
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-06-23 15:42:27 +0330

    create a unique folder for siteNav

---
+++ siteNavigation/siteNavigation.js
@@ -0,0 +1,7 @@
+const siteNavigation = `
+    <div class="siteNavigation">
+        <a href="#home">home</a>
+        <a href="#cart">cart</a>
+        <a href="#singlePage">single page</a>
+    </div>`;
+export default siteNavigation
