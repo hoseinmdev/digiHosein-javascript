77863bdd0dc1767355d930150568a8094824225e
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-06-24 19:01:34 +0330

    fix: refactore code

--- updateRoot.js
+++ updateRoot.js
@@ -1,16 +1,15 @@
 import { root } from "./index.js";
 import { routesPath } from "./routesPath.js";
+import fadeShow from "./utils/fadeShowElement.js";
 
 export const updateRoot = () => {
   const currentHash = location.hash.split("#")[1];
   if (!location.hash) location.hash = "#home";
   root.innerHTML = routesPath[currentHash];
-  // console.log(root.childNodes[1])
-  const childrenOfSiteLayout =
-    document.querySelector(".siteLayout").childNodes[3];
-  setTimeout(() => {
-    childrenOfSiteLayout.classList.remove("noneShow");
-  }, 150);
+
+  // FADE SHOW CHILD OF LAYOUT
+  const elementToFadeShow = document.querySelector(".siteLayout").childNodes[3];
+  fadeShow(elementToFadeShow, 300);
 };
 
 window.addEventListener("hashchange", updateRoot);
