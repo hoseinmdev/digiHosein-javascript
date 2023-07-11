16219037d54f417af3795d425ef722c4d12e8262
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-07-11 17:28:22 +0330

    feat: implement this for show products

---
+++ components/getProducts/getProducts.js
@@ -0,0 +1,70 @@
+import { allProducts } from "../../db.js";
+import numberToFa from "../../utils/NumberToFa.js";
+const productsContainer = document.createElement("div");
+productsContainer.classList.add("productsContainer");
+
+const renderProductPrice = (price, discount) => {
+  if (discount) {
+    const productDiscount = (price * discount) / 100;
+    const finalPrice = price - productDiscount;
+    return `
+    <p class=productPriceWithDiscount>
+        <div class="productPreviousPrice">${numberToFa(price)} تومان</div>
+        <div class="productNewPrice">${numberToFa(finalPrice)} تومان</div>
+        <div class="productDiscount">${discount}% تخفیف ویژه</div>
+    </p>`;
+  }
+  if (!discount) {
+    return `
+      <p class=productPrice>
+        ${numberToFa(price)} تومان
+      </p>`;
+  }
+};
+const getProducts = (category) => {
+  allProducts.forEach((p) => {
+    const product = document.createElement("div");
+    product.classList.add("productContainer");
+    product.setAttribute("id", p.id);
+    product.innerHTML = `
+        <img
+        id=${p.id}
+        class=imageStyle
+        src=${p.imageURL}
+        />
+        <div class=productInfoContainer>
+          <h4 class=productTitle data-id=${p.id}>
+              ${p.title}
+          </h4>
+          ${renderProductPrice(p.price, p.discount)}
+        </div>`;
+    productsContainer.append(product);
+  });
+  return productsContainer.outerHTML;
+};
+const productActions = () => {
+  const timeout = setTimeout(() => {
+    const imagesOfProducts = document.querySelectorAll(".imageStyle");
+    const titleOfProducts = document.querySelectorAll(".productTitle");
+    imagesOfProducts.forEach((p) =>
+      p.addEventListener("click", () => {
+        location.href = `#productPage-${p.id}`;
+        location.reload();
+      })
+    );
+    titleOfProducts.forEach((p) =>
+      p.addEventListener("click", () => {
+        location.href = `#productPage-${p.id}`;
+        location.reload();
+      })
+    );
+    clearTimeout(timeout);
+  }, 100);
+};
+window.addEventListener("hashchange", () => {
+  if (location.hash === "#home") {
+    productActions();
+  }
+});
+productActions();
+export default getProducts;
