6dab38d8cbabd3b0faeb7c137c2eb8dea389f5cc
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-07-11 17:23:28 +0330

    feat: update code

--- siteNavigation/siteNavigation.js
+++
@@ -1,7 +0,0 @@
-const siteNavigation = `
-    <div class="siteNavigation">
-        <a href="#home">home</a>
-        <a href="#cart">cart</a>
-        <a href="#singlePage">single page</a>
-    </div>`;
-export default siteNavigation
