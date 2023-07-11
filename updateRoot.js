825e266238fc927013bf19ded4721df04b172cd0
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-07-11 17:25:56 +0330

    fix: fix the hash bug

--- updateRoot.js
+++ updateRoot.js
@@ -1,15 +1,19 @@
 import { root } from "./index.js";
 import { routesPath } from "./routesPath.js";
 import fadeShow from "./utils/fadeShowElement.js";
+import scrollToUp from "./utils/scrollToUp.js";
 
 export const updateRoot = () => {
-  const currentHash = location.hash.split("#")[1];
+  let currentHash = location.hash.split("#")[1];
+  if (currentHash) {
+    currentHash = location.hash.split("#")[1].split("-")[0];
+  }
   if (!location.hash) location.hash = "#home";
   root.innerHTML = routesPath[currentHash];
-
   // FADE SHOW CHILD OF LAYOUT
-  const elementToFadeShow = document.querySelector(".siteLayout").childNodes[3];
-  fadeShow(elementToFadeShow, 300);
+  const elementToFadeShow = document.querySelector(".siteLayoutContent");
+  fadeShow(elementToFadeShow, 50);
+  scrollToUp();
 };
 
 window.addEventListener("hashchange", updateRoot);
