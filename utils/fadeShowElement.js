32a335270510cd24237d3837d7f636d9e15bf0d9
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-07-11 17:25:07 +0330

    feat: create this for show elements fade

--- utils/fadeShowElement.js
+++ utils/fadeShowElement.js
@@ -1,4 +1,5 @@
 const fadeShow = (elementToShow, timeToShow) => {
+  elementToShow.classList.add("noneShow");
   setTimeout(() => {
     elementToShow.classList.remove("noneShow");
   }, Number(timeToShow));
