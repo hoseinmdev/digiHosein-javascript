4f1fe383474bbfe209ceb983698ae3fd322ad921
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-07-11 17:18:32 +0330

    feat: site footer completed

---
+++ components/siteFooter/siteFooter.js
@@ -0,0 +1,69 @@
+const siteFooter = `<div class="siteFooter">
+      <div class="footerQuickAccess">
+        <div>
+          <span>دسترسی سریع</span>
+          <p>بلاگ</p>
+          <p>قیمت روز گوشی</p>
+          <p>گوشی سامسونگ</p>
+          <p>گوشی ایفون</p>
+          <p>گوشی شیائومی</p>
+          <p>خرید عمده گوشی</p>
+          <p>مقایسه گوشی</p>
+          <p>قیمت لپ تاپ</p>
+        </div>
+        <div>
+          <span>پرفروش ترین محصولات</span>
+          <p>شیائومی نوت 11</p>
+          <p>گوشی سامسونگ a32</p>
+          <p>گوشی سامسونگ a53</p>
+          <p>گوشی پوکو x3 پرو</p>
+          <p>گوشی سامسونگ a23</p>
+        </div>
+        <div>
+          <span>درباره ما</span>
+          <p>حسین محمودی در یک نگاه</p>
+          <p>اهداف و تعهد های ما</p>
+          <p>سرگذشت حسین محمودی</p>
+        </div>
+        <div>
+          <span>پیش از خرید</span>
+          <p>راهنمای خرید</p>
+          <p>راهنمای خرید قسطی</p>
+          <p>روش های خرید</p>
+          <p>ضمانت هفت روزه</p>
+          <p>شیوه و هزینه های ارسال</p>
+        </div>
+        <div>
+          <span>پس از خرید</span>
+          <p>تضمین ریجستری</p>
+          <p>رویه های بازگرداندن کالا</p>
+          <p>سوالات متداول ریجستری</p>
+          <p>رهگیری سفارش ها</p>
+        </div>
+        <div>
+          <span>قوانین و مقررات</span>
+          <p>قوانین و مقررات</p>
+          <p>حریم خصوصی کاربران</p>
+          <p>از زبان مشتریان</p>
+        </div>
+      </div>
+      <div class="contactUsAndSymbols">
+        <div class="contactUs"}>
+          <span>
+            <AiOutlineMail />
+            ایمیل : Hoseinmdev@gmail.com
+          </span>
+          <span>
+            <FiPhoneCall />
+            تلفن : 989222365685 +
+          </span>
+          <span>
+            اینستاگرام <BsInstagram />
+          </span>
+        </div>
+      </div>
+      <div class="footerDown">
+        <p>تمامی حقوق مادی معنوی این سایت متعلق به حسین محمودی می باشد</p>
+      </div>
+    </div>`;
+export default siteFooter;
