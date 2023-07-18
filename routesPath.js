0318703e5d1ca8b89dd810e1ef51918a9160c4fe
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-07-18 17:26:03 +0330

    add new route to site

--- routesPath.js
+++ routesPath.js
@@ -1,9 +1,11 @@
 import homePage from "./pages/homePage.js";
 import cartPage from "./pages/cartPage.js";
 import productPage from "./pages/productPage.js";
+import categorizedProductsPage from "./pages/categorizedProductsPage.js";
 
 export const routesPath = {
   home: homePage,
   cart: cartPage,
   productPage: productPage,
+  categories: categorizedProductsPage,
 };
