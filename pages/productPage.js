a583890e9e3d06aebbd3d3d303f4e2ebb360557b
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-07-18 14:20:29 +0330

    update imported files route

--- pages/productPage.js
+++ pages/productPage.js
@@ -1,6 +1,6 @@
 import scrollToUpButton from "../components/scrollToUpButton/scrollToUpButton.js";
-import productIntroduction from "../components/singleProductPage/productIntroduction.js";
-import productSpecifications from "../components/singleProductPage/productSpecifications.js";
+import productIntroduction from "../components/singleProductPage/productIntroduction/productIntroduction.js";
+import productSpecifications from "../components/singleProductPage/productSpecifications/productSpecifications.js";
 import siteLayout from "../layout/siteLayout.js";
 
 const productPage = siteLayout(
