afc6c93eb67a56a9fdd64a39d37c5a7304c553b2
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-07-20 16:48:41 +0330

    implement categorizedPage

--- pages/categorizedProductsPage.js
+++ pages/categorizedProductsPage.js
@@ -1,10 +1,15 @@
 import categorizedProducts from "../components/categorizedProductsPage/categorizedProducts/categorizedProducts.js";
+import categorizedProductsFilters from "../components/categorizedProductsPage/categorizedProductsFilters/categorizedProductsFilters.js";
 import siteLayout from "../layout/siteLayout.js";
 
 const categorizedProductsPage = siteLayout(
   `<div class="siteLayoutContent">
-    <div>
-      ${categorizedProducts()}
+    <div class="categorizedProductsPage">
+      ${categorizedProductsFilters()}
+      <div class="categorizedProductsBlock">
+        <p>- محصولات مورد نظر شما : </p>
+        ${categorizedProducts()}
+      </div>
     </div>
   </div>`
 );
