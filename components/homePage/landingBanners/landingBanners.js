8fef7abd8b12d46cc4308d53a8cfcec98b78580c
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-07-21 12:18:56 +0330

    add listeners

--- components/homePage/landingBanners/landingBanners.js
+++ components/homePage/landingBanners/landingBanners.js
@@ -1,8 +1,27 @@
+import changeHash from "../../../utils/changeHash.js";
+
 const landingBanners = () => {
   return `<div class="landingBannersContainer">
-        <img src="../../../assets/images/landing/gamingConsole.webp" />
-        <img src="../../../assets/images/landing/littleSpeaker.webp" />
+        <img src="../../../assets/images/landing/gamingConsole.webp" class="landingBanner" data-path="categories-1-consoles" />
+        <img src="../../../assets/images/landing/littleSpeaker.webp" class="landingBanner" data-path="categories-1-speakers" />
     </div>`;
 };
-
+const timeout = () => {
+  const timeout = setTimeout(() => {
+    const landingBanners = document.querySelectorAll(".landingBanner");
+    landingBanners.forEach((element) => {
+      element.addEventListener("click", (e) => {
+        changeHash(`${e.target.dataset.path}`);
+        location.reload();
+      });
+    });
+    clearTimeout(timeout);
+  }, 10);
+};
+window.addEventListener("hashchange", () => {
+  if (location.hash === "#home") {
+    timeout();
+  }
+});
+timeout()
 export default landingBanners;
