2daecac4e81659f69724877379a455269dd6c4f9
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-07-21 12:18:13 +0330

    add listeners

--- components/homePage/circleCategories/circleCategories.js
+++ components/homePage/circleCategories/circleCategories.js
@@ -1,3 +1,5 @@
+import changeHash from "../../../utils/changeHash.js";
+
 const categories = [
   {
     title: "گوشی موبایل",
@@ -30,22 +32,47 @@ const categories = [
     path: "#categories-1-speakers",
   },
 ];
+
 const categoriesContainer = document.createElement("div");
 categoriesContainer.classList.add("circleCategoriesContainer");
+
 const renderCategories = () => {
   categories.forEach((c) => {
     const categoryElement = document.createElement("div");
-    // categoryElement.textContent = c.title;
-    // categoryElement.href = c.path;
+    categoryElement.classList.add("circleCategoryContainer");
+    categoryElement.setAttribute("data-path", c.path);
     categoryElement.innerHTML = `<img class="circleCategoryImage" src=${c.imageUrl} />
     <p>${c.title}</p>`;
     categoriesContainer.append(categoryElement);
   });
 };
-renderCategories();
 
 const circleCategories = () => {
+  renderCategories();
+  timeout()
   const circleCategoriesJsx = `${categoriesContainer.outerHTML}`;
   return circleCategoriesJsx;
 };
+
+const timeout = () => {
+  const timeout = setTimeout(() => {
+    const circleCategoryContainers = document.querySelectorAll(
+      ".circleCategoryContainer"
+    );
+    circleCategoryContainers.forEach((element) => {
+      element.addEventListener("click", (e) => {
+        changeHash(`${e.currentTarget.dataset.path}`);
+        location.reload();
+      });
+    });
+    clearTimeout(timeout);
+  }, 10);
+};
+
+window.addEventListener("hashchange", () => {
+  if (location.hash === "#home") {
+    timeout();
+  }
+});
+
 export default circleCategories;
